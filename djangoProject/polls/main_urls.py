8abf1aac54ee3bc6484d720bc1dcc23704d3022d
Freddy Wang <13735572662@163.com>
2017-05-27 14:17:09 +0800

    delete

--- djangoProject/polls/main_urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-import views
-
-# from django.conf.urls import url
-# from . import views
-
-urlpatterns = [url(r'^polls/', include('polls.urls', namespace="polls")),]
