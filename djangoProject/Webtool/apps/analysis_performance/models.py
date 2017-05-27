2812cc0c9e5f0d3afc3d1f5e18cc4f3fe54c2805
Freddy Wang <13735572662@163.com>
2017-05-27 14:02:22 +0800

    delete

--- djangoProject/Webtool/apps/analysis_performance/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.db import models
-
-class SceneModel(models.Model):
-    scene_pic = models.ImageField(upload_to = 'analysis_performance_pic')
-    project_name=models.CharField(max_length=50)
-    scene_name = models.CharField(max_length=50)
-    scene_des = models.CharField(max_length=100)
-    date_time=models.CharField(max_length=50)
